c276da7680546acf8aff240cbf55e19b2ada0846
tomomonDQX <86160357+tomomonDQX@users.noreply.github.com>
2021-10-23 06:13:00 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./styles.css";
+
+export const App = () => {
+  return (
+    <>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTOTDO</p>
+        <ul>
+          <li className="list-row">
+            <p className="p-list">あああ</p>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTOTDO</p>
+        <ul>
+          <li className="list-row">
+            <p className="p-list">ううう</p>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
